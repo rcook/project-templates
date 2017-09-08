c28c0ea380ddaf03c68060da17e717ddca035355
Richard Cook <rcook@rcook.org>
2017-09-08 13:30:27 -0700

    Rename main scripts

--- script/ptool.cmd
+++ script/ptool.cmd
@@ -8,13 +8,13 @@ IFS=$'\n\t'
 
 script_dir=$(cd $(dirname $0); pwd -P)
 
-$script_dir/bootstrap $script_dir/project.py $*
+$script_dir/bootstrap $script_dir/ptool.py $*
 
 exit $?
 
 :CMDSCRIPT
 @echo off
-call "%~dp0bootstrap.cmd" "%~dp0project.py" %*
+call "%~dp0bootstrap.cmd" "%~dp0ptool.py" %*
 if errorlevel 1 (
     echo script failed
     exit /b 1
